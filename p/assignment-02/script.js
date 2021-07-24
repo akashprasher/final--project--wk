280aaf6702bbecfe01ce17cfab25d28be0e56076
akashprasher <prince.akash.prasher@gmail.com>
2021-07-24 12:53:53 +0530

    Adding files remote
    Signed-off-by: akashprasher <prince.akash.prasher@gmail.com>

---
+++ p/assignment-02/script.js
@@ -0,0 +1,7 @@
+var userName = prompt("Hey! Whats your name?");
+var laptopDesktop = prompt(
+  "Do you like laptop or desktop? (laptop or desktop)"
+);
+
+document.getElementById("usrName").innerHTML = userName;
+document.getElementById("textForUsr").innerHTML = laptopDesktop;
